08b009730620090bd312a04836e2a2f561a10f80
Rahil Rahimi <rahilrahimi1362@gmail.com>
2024-02-25 19:41:29 -0500

    modified html and js

--- index.js
+++ index.js
@@ -11,9 +11,14 @@ function clearDisplay(){
 
 function calculate(){
     try{
-    display.value = eval(display.value);
-    }
-    catch(error){
-        display.value = "Error";
-    }
+       // Check if the expression contains division by zero
+       if (display.value.includes('/0')) {
+          throw new Error('error:divide by 0');
+      }
+
+      // Evaluate the expression
+      display.value = eval(display.value);
+  } catch (error) {
+      display.value = error.message; // Display the error message
+  }
 }
